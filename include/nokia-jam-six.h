0284cea5867c7b22c2ee6d5e3032c05fbaac0ff9
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2024-02-17 00:59:39 +0100

    Initial project setup, starting window

---
+++ include/nokia-jam-six.h
@@ -0,0 +1,14 @@
+﻿
+#pragma once
+
+#include <iostream>
+
+#if defined(PLATFORM_DESKTOP)
+#define GLSL_VERSION            330
+#else   // PLATFORM_ANDROID, PLATFORM_WEB
+#define GLSL_VERSION            100
+#endif
+
+
+int WINDOW_WIDTH = 1280;
+int WINDOW_HEIGHT = 720;
---
+++ src/nokia-jam-six.cpp
@@ -0,0 +1,25 @@
+﻿// nokia-jam-six.cpp : Defines the entry point for the application.
+
+#include "nokia-jam-six.h"
+#include "raylib.h"
+
+using namespace std;
+
+
+int main()
+{
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - basic window");
+    
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
